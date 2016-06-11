a1b602ae40ffa8b6a05cb4bd79c9bf411d302e81
Eugene Mukhanov <eugene.mukhanov@audiencescience.com.com>
2016-06-11 13:41:56 +0300

    GenomicRangeQuery the best sulution.

--- src/main/java/org/eim/codility/genomicrangequery/Solution.java
+++ src/main/java/org/eim/codility/genomicrangequery/Solution.java
@@ -15,52 +15,95 @@ public class Solution {
         System.out.println("Res: "+res.toString());
     }
 
-    public int[] solution(String S, int[] P, int[] Q) {
-        char[] gen = S.toCharArray();
-        int[] res = new int[P.length];
-        int min;
-        for (int i = 0; i < P.length; i++) {
-            min = Integer.MAX_VALUE;
-            for (int j = P[i]; j <= Q[i]; j++) {
-                switch (gen[j]) {
-                    case 'A': min = 1;
-                        break;
-                    case 'C': if(2 < min) min = 2;
-                        break;
-                    case 'G': if(3 < min) min = 3;
-                        break;
-                    case 'T': if(4 < min) min = 4;
-                        break;
-                }
-                if (min == 1) break;
-            }
-            res[i] = min;
-        }
-        return res;
-    }
-
-//
+//  /**
+//   * Worked solution but very slowly
+//   * @param S
+//   * @param P
+//   * @param Q
+//   * @return
+//   */
 //    public int[] solution(String S, int[] P, int[] Q) {
 //        char[] gen = S.toCharArray();
 //        int[] res = new int[P.length];
+//
 //        int min;
 //        for (int i = 0; i < P.length; i++) {
 //            min = Integer.MAX_VALUE;
 //            for (int j = P[i]; j <= Q[i]; j++) {
-//                if (gen[j] == 'A') {
-//                    min = 1;
-//                    break;
-//                } else if (gen[j] == 'C') {
-//                    if (2 < min) min = 2;
-//                } else if (gen[j] == 'G') {
-//                    if (3 < min) min = 3;
-//                } else {
-//                    min = 4;
+//                switch (gen[j]) {
+//                    case 'A': min = 1;
+//                        break;
+//                    case 'C': if(2 < min) min = 2;
+//                        break;
+//                    case 'G': if(3 < min) min = 3;
+//                        break;
+//                    case 'T': if(4 < min) min = 4;
+//                        break;
 //                }
+//                if (min == 1) break;
 //            }
 //            res[i] = min;
 //        }
 //        return res;
 //    }
+//
+
+
+    /**
+     * Much better
+     * @param S
+     * @param P
+     * @param Q
+     * @return
+     */
+    public int[] solution(String S, int[] P, int[] Q) {
+        char[] gen = S.toCharArray();
+        int[] geni = new int[gen.length];
+
+        for (int i = 0; i < gen.length; i++) {
+            switch (gen[i]) {
+                case 'A': geni[i] = 1;
+                    break;
+                case 'C': geni[i] = 2;
+                    break;
+                case 'G': geni[i] = 3;
+                    break;
+                case 'T': geni[i] = 4;
+                    break;
+            }
+        }
+        int[] res = new int[P.length];
+        int[][] map = new int[4][geni.length];
+        int[] mapi = new int[4]; // max position of the A,C ... in map
+
+        int idx = 0;
+        for (int i = 0; i < geni.length; i++) {
+            idx = geni[i]-1;
+            map[idx][mapi[idx]++] = i;
+        }
+
+        for (int i = 0; i < P.length; i++) {
+            for (int j = P[i]; j <= Q[i]; j++) {
+
+                for (int k = 0; k < 4; k++) {
+                    for (int l = 0; l < mapi[k]; l++) {
+                        if ( P[i] <= map[k][l] && map[k][l] <= Q[i] ) {
+                            res[i] = (k+1);
+                            break;
+                        }
+                    }
+                    if (res[i] > 0) {
+                        break;
+                    }
+                }
+                if (res[i] > 0) {
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+
+
 
 }
---
+++ src/main/java/org/eim/codility/minavgtwoslice/Solution.java
@@ -0,0 +1,11 @@
+package org.eim.codility.minavgtwoslice;
+
+/**
+ * @author eim
+ * @since 2016-06-11
+ */
+public class Solution {
+
+
+
+}
