70820d4b129dd3f66e771f26fbcea87f6d22c2d9
Eugene Mukhanov <eugene.mukhanov@audiencescience.com.com>
2016-06-11 23:32:09 +0300

    Fish. The best solution.

--- src/main/java/org/eim/codility/lesson7/fish/Solution.java
+++ src/main/java/org/eim/codility/lesson7/fish/Solution.java
@@ -7,85 +7,62 @@ package org.eim.codility.lesson7.fish;
 public class Solution {
 
   public int solution(int[] A, int[] B) {
-    int f = -1;
+    int f = -1; // previous i value
     int sum=A.length;
     int jump=0;
-    for (int i = 0; i < B.length; i++) {
-      if (B[i] == -1) continue;
+    int i = 0;
+    int j = -1;
+
+    for (; i < B.length && j < B.length;) {
+
       if (B[i] == 1) {
         if (f > -1) jump++;
         f = i;
-      } else {
-        if (f < 0 ) {
+      }
+      if (B[i] == -1 || f < 0) {
+        i++;
+        continue;
+      }
+
+      if (i>=j) j = i+1;
+
+      for (; j < B.length;) {
+        if (B[j] == -1) {
+          j++;
           continue;
         }
-        for (int j = i; j < B.length; j++) {
-          if (B[j] == -1) continue;
-          if (B[j] == 1) {
-            if (f > -1) jump++;
-            f = -1;
-            break;
-          }
-            if(A[f] > A[j]) {
-              System.out.printf("%2d > eat  %2d\n",A[f],A[j]);
-              System.out.println(">>>>>>>>>>>>>>>>>>>>>>>");
-              B[j] = -1;
-              sum--;
-            } else {
-              sum--;
-              System.out.printf("%2d  eat < %2d\n",A[f],A[j]);
-              System.out.println("<<<<<<<<<<<<<<<<<<<<<<<");
-              B[f] = -1;
-              int b = f;
-              f = -1;
-              // search first "1" back if jump > 0
-              if (jump>0) {
-                for (; b >= 0; b--) {
-                  if (B[b] == 1) {
-                    i = b - 1;
-                    jump--;
-                    break;
-                  }
-                }
-              } else {
-                i = j - 1;
+        if (B[j] == 1) {
+          i++;
+          break;
+        }
+
+        sum--;
+
+        if(A[i] > A[j]) {
+          System.out.printf("%2d > eat  %2d\n",A[i],A[j]);
+          System.out.println(">>>>>>>>>>>>>>>>>>>>>>>");
+          B[j] = -1;
+          j++;
+        } else {
+          System.out.printf("%2d  eat < %2d\n",A[i],A[j]);
+          System.out.println("<<<<<<<<<<<<<<<<<<<<<<<");
+          B[i] = -1;
+          f = -1;
+          // search first "1" back if jump > 0
+          if (jump>0) {
+            for (;i >= 0;i--) {
+              if (B[i] == 1) {
+                jump--;
+                break;
               }
-              break;
             }
+          } else {
+            i = j + 1;
+          }
+          break;
         }
       }
     }
     return sum;
   }
-
-//  public int solution(int[] A, int[] B) {
-//    int f = -1, s = -1;
-//    int sum=A.length;
-//    s = B.length>1?1:0;
-//    f = 0;
-//    while (true) {
-//      if (f == -1 || s == -1) break;
-//        if (B[f] > B[s]) { //
-//          if (A[f] > A[s]) {
-//            // A[i] eats A[i+1]. A[i] survived
-//            A[s] = -1;
-//            B[s] = A[s];
-//            sum--;
-//            s = getNext(B,s);
-//          } else {
-//            // A[i+1] eats A[i]. A[i+1] survived
-//            A[f] = -1;
-//            B[f] = A[f];
-//            sum--;
-//            f = getNext(B,s+1);
-//            s = getNext(B,f+1);
-//          }
-//        } else {
-//          f = getNext(B,s);
-//          s = getNext(B,f+1);
-//        }
-//    }
-//    return sum;
-//  }
-
 }
